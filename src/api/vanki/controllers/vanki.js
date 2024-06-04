80cc4e140363dadca591ea1c394cc3576e2b511a
Sandeep <sandbro7163337@gmail.com>
2024-06-04 12:59:52 +0530

    vanki schema add

---
+++ src/api/vanki/controllers/vanki.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vanki controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::vanki.vanki');
---
+++ src/api/vanki/routes/vanki.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vanki router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::vanki.vanki');
